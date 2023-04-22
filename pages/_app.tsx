2067fb73eaa1e830907dcc8fde110579d201f0d9
LimjeongSik <ol37@naver.com>
2023-04-22 19:13:58 +0900

    feat:header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/font.css";
 
 import Layout from "@/components/layout/Layout";
-import Header from "@/components/layout/header";
+import Header from "@/components/HeaderComponent";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
